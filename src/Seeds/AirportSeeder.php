8d1f11350f0adf993ef25018ddae1b4b0fbba8fd
Dyan Galih <dyan.galih@gmail.com>
2019-02-26 23:59:59 +0700

    fix typo on file

--- src/Seeds/AirportSeeder.php
+++ src/Seeds/AirportSeeder.php
@@ -18,7 +18,7 @@ class AirportSeeder extends Seeder
 {
     public function run(AirportRepository $airportRepository, AirportParam $airportParam)
     {
-        $file = __DIR__ . '/../Resources/Csv/airports.csv';
+        $file = __DIR__ . '/../Resources/Csv/Airports.csv';
         $header = array('id', 'ident', 'type', 'name', 'latitude_deg', 'longitude_deg', 'elevation_ft', 'continent', 'iso_country', 'iso_region', 'municipality', 'scheduled_service', 'gps_code', 'iata_code', 'local_code', 'home_link', 'wikipedia_link', 'keywords');
         
         $delimiter = ',';
